b9fc3c10defce4f72647f4cc34fa605c97fcb249
Amar Yasir <3marofficial249@gmail.com>
2025-02-25 17:05:33 +0200

    optimizaitions

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React from 'react'
 import '../sections/Features/Features.css';
 
 export interface FeatureProps {
@@ -11,7 +11,7 @@ export interface FeatureProps {
 const Feature: React.FC<FeatureProps> = ({ icon, title, description }) => {
     return (
         <div className='feature d-flex justify-content-around flex-column bg-light p-4 shadow'>
-            <img src={icon} alt="" loading='lazy'/>
+            <img src={icon} alt="" loading='lazy' />
             <h3 className='my-3'>{title}</h3>
             <p className='text-muted'>{description}</p>
         </div>
