86457e03e18926a0ca0d409ef4f47dafb05003e0
Amar Yasir <3marofficial249@gmail.com>
2025-03-07 04:20:27 +0200

    ...

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -15,30 +15,31 @@ const Signup: React.FC = () => {
     return (
         <>
             <div id='signup' className='min-h-screen flex flex-col justify-center items-center p-5 shadow'>
-                <div style={{ minHeight: '75vh', minWidth: '75vw' }} className="relative bg-white flex flex-col justify-center items-center p-3 border rounded-4 shadow">
-                    <div className="brand flex justify-center items-center">
-                        <img src={gglifeLogo} alt="⚡" loading='lazy' className='w-5'/>
-                        <h3 className='font-bold'>GGLife</h3>
+                <div className="relative min-w-[75vw] min-h-[75vh] bg-[#f8f9fa] flex flex-col justify-center items-center p-3 rounded-2xl shadow">
+                    <div className="brand flex justify-center items-center my-2">
+                        <img src={gglifeLogo} alt="⚡" loading='lazy' className='w-6' />
+                        <h3 className='font-bold text-3xl'>GGLife</h3>
                     </div>
-                    <form action="" method='POST' className='w-[80%]' onSubmit={(e) => e.preventDefault()}>
-                        <div className="input-group my-2">
-                            <label htmlFor="name" className='mb-1'><img src={usernameImg} alt="" width={'20px'} className='mx-2' />Your name</label>
-                            <input type="text" name="name" id="name" className='form-control rounded-4 w-100 ' placeholder='Ahmed, Ali, etc... ' />
+                    <form action="" method='POST' className='w-[80%] py-2' onSubmit={(e) => e.preventDefault()}>
+                        <div className="my-4">
+                            <label htmlFor="name" className='mb-1 flex '><img src={usernameImg} alt="" className='mx-2 w-5' />Your name</label>
+                            <input type="text" name="name" id="name" className='bg-white p-2 shadow rounded-2xl w-full focus:ring-1 focus:shadow-2xl transition-all' placeholder='Ahmed, Ali, etc... ' required />
                         </div>
-                        <div className="input-group my-2">
-                            <label htmlFor="email" className='mb-1'><img src={emailImg} alt="" width={'20px'} className='mx-2' />Your email</label>
-                            <input type="email" name="email" id="email" className='form-control rounded-4 w-100 ' placeholder='user@example.com, etc...' />
+                        <div className="my-4">
+                            <label htmlFor="email" className='mb-1 flex'><img src={emailImg} alt="" className='mx-2 w-5' />Your email</label>
+                            <input type="email" name="email" id="email" className='bg-white p-2 shadow rounded-2xl w-full focus:ring-1 focus:shadow-2xl transition-all' placeholder='user@example.com, etc...' required />
                         </div>
-                        <div className="input-group my-2">
-                            <label htmlFor="password" className='mb-1'><img src={passwordImg} alt="" width={'20px'} className='mx-2' />Your password</label>
-                            <input type="password" name="password" id="password" className='form-control rounded-4 w-100 ' placeholder='San$2001, 3ll200ii, etc...' ref={passwordInputRef} />
+                        <div className="my-4 relative">
+                            <label htmlFor="password" className='mb-1 flex'><img src={passwordImg} alt="" className='mx-2 w-5' />Your password</label>
+                            <input type="password" name="password" id="password" className='bg-white p-2 shadow rounded-2xl w-full focus:ring-1 focus:shadow-2xl transition-all' placeholder='San$2001, 3ll200ii, etc...' ref={passwordInputRef} required />
                             {
-                                visible ? (<img src={eyeSlashImg} alt="" style={{ width: '20px', opacity: '.5', cursor: 'pointer', position: 'absolute', right: '10px', top: '37px', zIndex: '99' }} onClick={handlePassword} />) :
-                                    (<img src={eyeImg} alt="" style={{ width: '20px', opacity: '.5', cursor: 'pointer', position: 'absolute', right: '10px', top: '37px', zIndex: '99' }} onClick={handlePassword} />)
+                                visible ? (<img src={eyeSlashImg} alt="" className='w-5 opacity-50 cursor-pointer absolute right-3 bottom-2 z-[99]' onClick={handlePassword} />) : (
+                                    <img src={eyeImg} alt="" className='w-5 opacity-50 cursor-pointer absolute right-3 bottom-2 z-[99] ' onClick={handlePassword} />
+                                )
                             }
                         </div>
-                        <button className='btn btn-dark w-100 rounded-4 my-3'>Register →</button>
-                        <a href="/login">Already have an account? <span className='text-primary'>Login</span></a>
+                        <button className='bg-black text-white w-full rounded-2xl p-2 my-3 hover:-translate-y-1 transition-all cursor-pointer'>Register →</button>
+                        <a href="/login">Already have an account? <span className='text-blue-500'>Login</span></a>
                     </form>
                 </div>
             </div>
