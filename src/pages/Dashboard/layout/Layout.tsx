ea28da2a9fb6e1c285a2f30d95b8c3d57c971f58
Amar Yasir <3marofficial249@gmail.com>
2025-03-10 01:20:33 +0200

    fixed some stuff

--- src/pages/Dashboard/layout/Layout.tsx
+++ src/pages/Dashboard/layout/Layout.tsx
@@ -6,9 +6,9 @@ const Layout: React.FC = () => {
     const { theme } = useTheme();
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     return (
-        <div id='dashboard' className={`min-h-screen flex transition-all ${theme}`}>
+        <div id='dashboard' className={`min-h-screen flex ${theme}`}>
             <Sidebar isClicked={isSidebarOpen} setIsClicked={setIsSidebarOpen} />
-            <div className={`main-content px-3 py-2 grow ml-0 flex flex-col mr-auto lg:ml-[250px] ${theme==='dark'?'bg-[#232333]':''}`}>
+            <div className={`main-content px-3 py-2 grow ml-0 flex flex-col mr-auto transition-all duration-300 lg:ml-[250px] ${theme==='dark'?'bg-[#232333]':''}`}>
                 <Dashbar isClicked={isSidebarOpen} setIsClicked={setIsSidebarOpen} />
                 <Content />
             </div>
