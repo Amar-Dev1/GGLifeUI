67cc3b556f75a61436ba54449bf4a14c2d559b62
Amar Yasir <3marofficial249@gmail.com>
2025-03-12 01:44:06 +0200

    fixing contact section

--- src/components/GoUp.tsx
+++ src/components/GoUp.tsx
@@ -5,7 +5,7 @@ interface GoUpProps {
 }
 const GoUp: React.FC<GoUpProps> = ({ status, onclick }) => {
     return (
-        <div className={`fixed bottom-5 right-5 bg-transparent border-none outline-none w-14 p-3.5 rounded-full flex justify-center items-center cursor-pointer z-[999] shadow-md hover:-translate-y-1 transition-all ${status ? 'visible' : 'invisible'} `} onClick={onclick}>
+        <div className={`fixed bottom-5 right-5 bg-transparent shadow-md border-2 border-gray-300 w-10 p-2 rounded-full flex justify-center items-center cursor-pointer z-[999] hover:-translate-y-1 transition-all ${status ? 'visible' : 'invisible'} `} onClick={onclick}>
             <img src={arrowUp} alt="" className='w-full' />
         </div>
     )
